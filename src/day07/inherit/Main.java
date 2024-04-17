abf8c5890d8cf883ddc804a02c4e714f0896fb69
yun.jeongg <bae.yunjeongg.gmail.com>
2024-04-17 21:53:12 +0900

    # day 07

---
+++ src/day07/inherit/Main.java
@@ -0,0 +1,13 @@
+package day07.inherit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+        Computer com = new Computer();
+
+        System.out.println("계산기로 구한 원의넓이: " + cal.calcAreaCircle(5));
+        System.out.println("컴퓨터로 구한 원의넓이: " + com.calcAreaCircle(5));
+    }
+}
---
+++ src/day07/inherit/product/Television.java
@@ -0,0 +1,19 @@
+package day07.inherit.product;
+
+public class Television extends ElectronicProduct {
+
+    int screenSize;
+
+    public Television(String productName, double price, String manufacturer, int screenSize) {
+        this.productName = productName;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.screenSize = screenSize;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.printf("Screen Size: %d inches\n", this.screenSize);
+    }
+}
